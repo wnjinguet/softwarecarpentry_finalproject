2691793965714036db12a907f98a70bd344a1475
wnjinguet <93890598+wnjinguet@users.noreply.github.com>
2023-05-06 20:16:43 -0400

    Update solver.py

--- solver.py
+++ solver.py
@@ -9,3 +9,17 @@ def display_score():
   pass
 def play_again():
   pass
+
+
+question = {
+"A very high value of K indicates that"
+"What is the pH of a 10–5 M KOH solution?"
+"Active metals react with certain acids, such as hydrochloric acid, to yield a metal compound and"
+"How many moles of ions are produced by the dissociation of 1 mol of MgCl2?"
+"For the reaction represented by the equation 2H2 + O2 → 2H2O, how many grams of water are produced from 6.00
+mol of hydrogen?"
+"Which of the following is an electrolyte?"
+"Which reaction does not occur?"
+"Enthalpy change is the"
+"If the concentration of reactants is higher"
+"If the concentration of reactants is higher"
