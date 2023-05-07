51d95eb10dcc8380fa72e73233f276591e331869
wnjinguet <93890598+wnjinguet@users.noreply.github.com>
2023-05-06 20:03:52 -0400

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,1 +1,11 @@
-first we define the different functions that will be used in this game.
+#The goal of this code is to create and run a quiz game in python using some key concepts learn throughout the semester
+#First we define the different functions that will be used in this game.
+
+def new_game(): 
+  pass 
+def check_answer():
+  pass
+def display_score():
+  pass
+def play_again():
+  pass
