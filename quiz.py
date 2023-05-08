aa87c563c8dcb0acbac2334a9a2847437dc255ba
wnjinguet <93890598+wnjinguet@users.noreply.github.com>
2023-05-07 21:17:15 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,8 +1,9 @@
 # The goal of this code is to create and run a quiz game in python using some key concepts learned throughout the semester
 # First I define the different functions that will be used in this game.
 # Later I create a library and answer choices for the game.
-# Afterwards, the there is a portion that reveals the user's score. Base on that score, they either a celebratory message or a message prompting them tocontinue to ractice, base on their score. 
-# After several trials, the user's scores are graphed to visually show progress 
+# Afterwards, there is a portion that reveals the user's score. Based on that score, they either get a celebratory message or a message prompting them to continue to practice, based on their score. 
+# After several trials, the user's scores are graphed to visually show progress.
+
 import matplotlib.pyplot as plt
 
 def new_game():
